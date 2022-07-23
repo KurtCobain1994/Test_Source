1580e0e3f8878c85a9c84faa1f8fda98fde8fd2f
KurtCobain1994 <bobvanh@hotmail.nl>
2022-07-23 07:17:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Test_Source
+
+Developed with Unreal Engine 5
---
+++ Source/Test_Source/Test_SourceGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Test_SourceGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEST_SOURCE_API ATest_SourceGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
